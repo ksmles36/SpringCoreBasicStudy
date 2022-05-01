df8324bcdf2f393d1fa7aad6379a77065789350c
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2022-05-01 21:42:41 +0900

    컴포넌트 스캔과 의존관계 자동주입 @ComponentScan @Autowired 실습

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+//@Component("memberService2")  //이런식으로 스프링 빈의 이름을 지정해줄 수도 있음, 생략하면 기본 네이밍 전략으로 감
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
---
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,20 @@
+package hello.core.scan;
+
+import hello.core.member.MemberService;
+import hello.core.AutoAppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberServiceBean = ac.getBean(MemberService.class);
+        assertThat(memberServiceBean).isInstanceOf(MemberService.class);
+    }
+
+}
