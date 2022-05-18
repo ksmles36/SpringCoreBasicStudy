a7f5112d19ee26b5aaa726ba2b9807159b2c1214
ksmles36 <ksmles36@gmail.com>
2022-05-18 16:54:46 +0900

    조회한 빈이 모두 필요할 때, List, Map

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
@@ -31,7 +32,7 @@ public class OrderServiceImpl implements OrderService {
     */
 
     @Autowired  //생성자 주입 (생성자가 하나밖에 없으면 @Autowired 어노테이션 생략 가능)
-    public OrderServiceImpl(MemberRepository memberRepository, /*@Qualifier("mainDiscountPolicy")*/ DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, /*@Qualifier("mainDiscountPolicy")*/ @MainDiscountPolicy DiscountPolicy discountPolicy) {
 //        System.out.println("memberRepository = " + memberRepository);
 //        System.out.println("discountPolicy = " + discountPolicy);
         this.memberRepository = memberRepository;
